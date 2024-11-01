b84ea6aa74b8c75b5719a2f44d03d275ce7225d9
gireesh27 <kasagireesh@gmail.com>
2024-11-02 01:31:59 +0530

    all done

--- app/page.js
+++ app/page.js
@@ -3,9 +3,10 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div>
-      <h1>
-      Open the Employees Page
+    <div className=' w-full h-ful overflow-y-hidden border-black'>
+      <h1 className='text-center text-5xl mx-auto mt-72 border-x-violet-600 px-12 py-12 rounded-xl '>
+      Employees Page: 
+      <a href="http://localhost:3000/employees" >Open</a>
       </h1>
     </div>
   );
