d642ceceac729824863cc4334446a51d82c5f92f
gireesh27 <kasagireesh@gmail.com>
2024-11-02 12:05:40 +0530

    removed quotes

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
       <h1 className='text-center text-4xl mx-auto mt-10 border-x-violet-600 px-12 rounded-xl '>Click on 
       <a href="https://employee-data-five.vercel.app/employees" className='font-bold text-green-700'> Open </a>
       </h1>
-      <h2> <strong>"/employees"</strong>     is the navigated root for this project</h2>
+      <h2> <strong>/employees</strong>     is the navigated root for this project</h2>
       
       <h2 className='my-4'>open <strong> Github </strong> for this project 
         <a href='https://github.com/gireesh27/EdTech-website' className='text-lg'> 🔗 </a>
@@ -15,7 +15,7 @@ export default function Home() {
       <hr/>
       <div className=" flex flex-row space-x-5 bottom-full  right-0 font-serif font-extrabold text-base ">
       <div>Developed by </div>
-      <div className='font-extrabold text-sky-700 '>Gireesh</div>
+      <div className='font-extrabold text-sky-700 '> Gireesh </div>
       </div>
     </div>
   );
